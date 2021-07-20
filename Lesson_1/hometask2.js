808058f8ec2f025e29874dfd7b06db090e294a13
keyjey101 <keyjey.danilov@gmail.com>
2021-07-20 22:26:34 +0500

    changes after testing

--- Lesson_1/hometask2.js
+++ Lesson_1/hometask2.js
@@ -5,20 +5,20 @@ function counter(num = 1) {
   let addValue = 2;
 
   return () => {
+    if (!current) return (current += addValue);
     if (current && current % 5 === 0) {
-      current = current / 5;
       addValue = 3;
+      return (current = current / 5);
     }
     if (current && current % 7 === 0) {
-      current -= 7;
       addValue = 1;
+      return (current -= 7);
     }
     current += addValue;
     return current;
   };
 }
-
-const number = counter(1);
+const number = counter(2);
 
 console.log(number());
 console.log(number());
